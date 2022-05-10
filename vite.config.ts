dfee563a27b311bbe99e6c17b78a4ffff8b29a2b
wuxudongxd <wuxudongxd@gmail.com>
2022-05-10 22:31:11 +0800

    ✨ feat: 添加 alias

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "~": path.resolve(__dirname, "./src"),
       src: path.resolve(__dirname, "./src"),
       assets: path.resolve(__dirname, "./src/assets"),
       components: path.resolve(__dirname, "./src/components"),
