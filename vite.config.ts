faf1e7d90fdbfb4e408839feb177d79dd3c1667f
wuxudongxd <wuxudongxd@gmail.com>
2022-05-11 15:47:34 +0800

    🎈 perf: 升级依赖，替换 tailwindcss 为 windcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import WindiCSS from "vite-plugin-windicss";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), WindiCSS()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
