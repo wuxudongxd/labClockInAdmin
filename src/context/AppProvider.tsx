022f737106644a27345d9278062c23d38e8e115a
wuxudongxd <wuxudongxd@gmail.com>
2022-05-21 21:23:58 +0800

    🦄 refactor: 更改类型

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
  * 使用react query进行服务端数据的缓存、更新、组件间共享
  */
 // 提供 QueryClientProvider
-const AppProvider: FC = ({ children }) => {
+const AppProvider = ({ children }: PropsWithChildren<{}>) => {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
