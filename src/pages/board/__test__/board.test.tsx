90e54e6c12a580e15bcb2607550fe967b2f68443
wuxudongxd <wuxudongxd@gmail.com>
2022-05-22 21:51:46 +0800

    ✨ feat: vitest 测试学习

--- src/pages/board/__test__/board.test.tsx
+++ src/pages/board/__test__/board.test.tsx
@@ -1,34 +1,26 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, vi } from "vitest";
 import AppProvider from "~/context/AppProvider";
-import mountTest from "~/test/mountTest";
 
 import Border from "..";
 
 describe("Border working test", () => {
+  // 这里使用mock而不是spyOn,是因为如果只模拟unAuditUserQuery函数，~/pages/unAudit/hooks内导入db的代码仍会执行，执行db初始化代码会报错
   vi.mock("~/pages/unAudit/hooks", () => {
     return {
+      __esModule: true,
       default: () => ({ unAuditUserQuery: { data: [] }, auditUser: () => {} }),
     };
   });
   vi.mock("~/pages/lab/hooks", () => {});
   vi.mock("~/pages/leave/hooks", () => {});
 
-  mountTest(() => (
-    <AppProvider>
-      <Border />
-    </AppProvider>
-  ));
-
-  it("should work", () => {
+  it("render correctly", () => {
     render(
       <AppProvider>
         <Border />
       </AppProvider>
     );
-    expect(
-      screen.getByText("Ant Design ©2018 Created by Ant UED")
-    ).toBeInTheDocument();
-    expect(screen.getByText("待审核用户")).toMatchSnapshot();
+    expect(screen.getByRole("menu")).toMatchSnapshot();
   });
 });
